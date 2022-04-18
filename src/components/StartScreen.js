9376e3c41169c84a7dc9dce4741ac5a4fcccc666
thiago <thiagooffice2@gmail.com>
2022-04-18 19:46:10 -0300

    Create initial project

---
+++ src/components/StartScreen.js
@@ -0,0 +1,11 @@
+import './StartScreen.css'
+
+export default function StartScreen(){
+    return(
+        <div className='start'>
+            <h1>Secret Word</h1>
+            <p>Clique no botão abaixo para jogar</p>
+            <button>Começar o jogo</button>
+        </div>
+    )
+}
---
+++ src/data/words.js
@@ -0,0 +1,7 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Antena"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    programação: ["Linguagem","Framework", "Javascript", "React"],alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"]
+}
