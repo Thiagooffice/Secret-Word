6b41a16de80f3efcc8aa82475b312ce66d9b778c
thiago <thiagooffice2@gmail.com>
2022-04-19 17:27:51 -0300

    stages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import './App.css';
 import StartScreen from './components/StartScreen';
+import {useCallback, useEffect, useState} from 'react'
+import { wordsList } from './data/words'
+import Game from './components/Game';
+import GameOver from './components/GameOver'
+
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"}
+]
 
 function App() {
+
+  const [gameStage, setGameStage]=useState(stages[0].name)
+  const [words] = useState(wordsList)
+
+  //starts the secret word game
+  const startGame = () =>{
+    setGameStage(stages[1].name)
+  }
+
+  //process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name)
+  }
+
+  //start the game
+  const retry = () => {
+    setGameStage(stages[0].name)
+  }
+
   return (
     <div className="App">
-      <StartScreen/>
+      {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "end" && <GameOver retry={retry} />}
     </div>
   );
 }
---
+++ src/components/GameOver.js
@@ -0,0 +1,10 @@
+import './GameOver.css'
+
+export default function GameOver({retry}){
+    return(
+        <div>
+            <h1>Game Over</h1>
+             <button onClick={retry} >Resetar Jogo</button>
+        </div>
+    )
+}
