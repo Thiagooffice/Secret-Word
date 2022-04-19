7c75dd13935624ea31b52dbb2617f8cf3956c447
thiago <thiagooffice2@gmail.com>
2022-04-19 17:59:26 -0300

    choosing random word and category

--- src/App.js
+++ src/App.js
@@ -16,9 +16,45 @@ function App() {
   const [gameStage, setGameStage]=useState(stages[0].name)
   const [words] = useState(wordsList)
 
+  const [pickedWord, setPickedWord]=useState("")
+  const [pickedCategory, setPickedCategory] = useState("")
+  const [letters, setLetters] = useState([])
+
+  const pickWordAndCategory = () => {
+    //pick a random category
+    const categories = Object.keys(words)
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
+
+    console.log(category);
+
+    //pick a random word
+    const word = words[category][Math.floor(Math.random() * words[category].length)]
+
+    console.log(word);
+
+    return {word, category}
+
+  }
+
+
   //starts the secret word game
   const startGame = () =>{
-    setGameStage(stages[1].name)
+    //pick word and pick category
+    const { word, category } = pickWordAndCategory()
+
+    //create an array of letter
+    let wordLetters = word.split("")
+
+    wordLetters = wordLetters.map((L) => L.toLowerCase())
+
+    console.log(wordLetters);
+    console.log(word, category);
+
+    //fill states
+    setPickedWord(word)
+    setPickedCategory(category)
+    setLetters(letters)
+
   }
 
   //process the letter input
